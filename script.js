b088c8fba442f89c248e66f0ffc84c0b6b9317a2
Rishabh Jajoriya <77231523+rishabhjajoriya@users.noreply.github.com>
2024-10-16 22:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+async function getArray(){
+	let array = [1,2,3,4]
+	return array
+	
+} 
+
+
+getArray().then((array)=>{
+	let evens = array.filter((i)=>i%2===0)
+    
+    setTimeout(()=>{
+      let el = document.getElementById('output');
+      el.innerHTML = evens.join(',')
+    },1000)
+    
+	return evens
+}).then((evens)=>{
+  let twos = evens.map((i)=>i*2)
+  setTimeout(()=>{
+    let el = document.getElementById('output');
+      el.innerHTML = twos.join(',')
+  },2000)
+})
